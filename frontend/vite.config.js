477f5519f5d6b59ec176f83c7a00eb0a82024fdf
Vimalesh <vimalesh@trainery.one>
2026-02-16 00:33:38 +0530

    build fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,12 @@
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // keep root if deployed at domain root
+  build: {
+    outDir: 'dist', // default
+  },
   server: {
     hmr: false,
   },
